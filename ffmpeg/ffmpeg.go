e473c1763a442480a94ea039e147ef50c2dd765f
Joey Kuhnsman <55155882+musicmank545@users.noreply.github.com>
2023-11-27 17:21:35 -0500

    Update ffmpeg.go

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -698,7 +698,7 @@ func createCOutputParams(input *TranscodeOptionsIn, ps []TranscodeOptions) ([]C.
 		if len(p.VideoEncoder.Name) <= 0 && len(p.VideoEncoder.Opts) <= 0 {
 			p.VideoEncoder.Opts = map[string]string{
 				"forced-idr": "1",
-				"preset":     "slow",
+				"preset":     "fast",
 				"tier":       "high",
 			}
 			if p.Profile.Quality != 0 {
